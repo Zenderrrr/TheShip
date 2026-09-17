24163156af0ec0e860d63794216a77a9c8734ae5
Egor <125749439+Zenderrrr@users.noreply.github.com>
2026-09-17 12:50:59 +0200

    fix hard coded api

--- ship/config.py
+++ ship/config.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import config
 
 # Load SHIP_HOST from .env file if present
 for env_path in [pathlib.Path.cwd() / ".env", pathlib.Path(__file__).parent.parent / ".env"]:
@@ -12,7 +13,7 @@ for env_path in [pathlib.Path.cwd() / ".env", pathlib.Path(__file__).parent.pare
         break
 
 # Spacecraft IP Address
-HOST = os.getenv("SHIP_HOST", "192.168.103.40")
+HOST = os.getenv("SHIP_HOST", host=config.HOST)
 
 # Known Station Coordinates (X, Y)
 STATIONS = {
